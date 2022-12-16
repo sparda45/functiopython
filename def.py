7414c59215e5f285260235897b49828b29ff1eab
reflyn andyaz <62463844+sparda45@users.noreply.github.com>
2022-12-16 15:51:28 +0800

    Add files via upload

---
+++ def.py
@@ -0,0 +1,11 @@
+hewan, jenis = 'singa', 'carnivore'
+
+def animal ():
+    hewan = 'trex'
+    print(hewan, jenis)
+    
+print('PANGGIL FUNGSI ANIMAL')
+animal()
+
+print('PANGGIL LANGSUNG')
+print(hewan, jenis)
